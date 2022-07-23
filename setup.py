2224a7db536a2c52c197bd726d0454a4cddd0e61
aminediro <aminedirhoussi1@gmail.com>
2022-07-23 15:06:58 +0200

    setup projects

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from os import path
+import os
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+
+description = 'daskqueue distributed queue package'
+
+long_description = description
+
+version = '0.1.0'
+
+setup(
+  name = 'daskqueue', # Name of the package
+  packages = ['daskqueue'], # name of the folder containing the package (ideally keep it same as the package name)
+  version = version,
+  license='MIT',        
+  description = description,
+  long_description=long_description,
+  long_description_content_type="text/x-rst",
+  author = 'Amine Dirhoussi',
+  keywords = ['Distributed Task Queue'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)

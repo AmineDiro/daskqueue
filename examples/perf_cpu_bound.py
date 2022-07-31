4cc3ec82057e6e92c3bc5ba58086461793bd5560
aminediro <aminedirhoussi1@gmail.com>
2022-07-31 16:49:50 +0200

    update examples new interface

--- examples/perf_cpu_bound.py
+++ examples/perf_cpu_bound.py
@@ -1,35 +1,40 @@
 import time
 import os
-from distributed import Client, LocalCluster, Actor
-from daskqueue import QueuePool, ConsumerBaseClass, ConsumerPool
+from distributed import Client
+from daskqueue import QueuePool, ConsumerPool
 from daskqueue.utils import logger
 
 
 def process_item():
-    return sum(i * i for i in range(10**4))
+    return sum(i * i for i in range(10**8))
 
 
 if __name__ == "__main__":
+
     client = Client(
-        n_workers=3,
+        n_workers=5,
         threads_per_worker=1,
-        dashboard_address=":8787",
+        dashboard_address=":3338",
         direct_to_workers=True,
     )
 
-    n_consumers = 1
+    ## Params
+    n_queues = 1
+    n_consumers = 5
 
-    queue_pool = client.submit(QueuePool, n_queues=1, actor=True).result()
-    # TODO : QueuePool interface, .submit()
-
-    dashboard_port = client.dashboard_link.split(":")[-1]
-
-    logger.info(f" Dashboard link : http://localhost:{dashboard_port}")
+    queue_pool = QueuePool(client, n_queues)
 
     consumer_pool = ConsumerPool(client, queue_pool, n_consumers=n_consumers)
+
+    print(queue_pool)
+    print(consumer_pool)
+
     consumer_pool.start()
 
-    for i in range(10):
+    for i in range(5):
         queue_pool.submit(process_item)
 
     consumer_pool.join()
+
+    print(queue_pool)
+    print(consumer_pool)
--- examples/perf_io_bound.py
+++ examples/perf_io_bound.py
@@ -7,26 +7,37 @@ from daskqueue import QueuePool, ConsumerBaseClass, ConsumerPool
 from daskqueue.utils import logger
 
 
-def process_item():
+def open_file():
     time.sleep(1)
-    logger.debug("[Executor] processing ")
     with open("/dev/urandom", "rb") as f:
         return f.read(100)
 
 
 if __name__ == "__main__":
-    client = Client(n_workers=3, threads_per_worker=1)
+    client = Client(
+        n_workers=5,
+        threads_per_worker=1,
+        dashboard_address=":3338",
+        direct_to_workers=True,
+    )
 
-    queue_pool = client.submit(QueuePool, 1, actor=True).result()
+    ## Params
+    n_queues = 1
+    n_consumers = 2
 
-    ## DEBUG
-    q = queue_pool.get_max_queue().result()
+    queue_pool = QueuePool(client, n_queues)
 
-    n_consumers = 1
-    consumer_pool = ConsumerPool(client, queue_pool, n_consumers=1)
+    consumer_pool = ConsumerPool(client, queue_pool, n_consumers=n_consumers)
+
+    print(queue_pool)
+    print(consumer_pool)
+
     consumer_pool.start()
 
-    for i in range(10):
-        queue_pool.submit(process_item)
+    for i in range(5):
+        queue_pool.submit(open_file)
 
     consumer_pool.join()
+
+    print(queue_pool)
+    print(consumer_pool)

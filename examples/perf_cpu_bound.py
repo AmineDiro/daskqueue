730df0844941610fa65d38520d42629d0516ffaf
aminediro <aminedirhoussi1@gmail.com>
2022-08-07 15:32:59 +0200

    publish on release

--- examples/perf_cpu_bound.py
+++ examples/perf_cpu_bound.py
@@ -1,8 +1,13 @@
+import queue
 import time
 import os
 from distributed import Client
 from daskqueue import QueuePool, ConsumerPool
 from daskqueue.utils import logger
+
+
+def inc(x):
+    return x + 1
 
 
 def process_item():
@@ -12,15 +17,15 @@ def process_item():
 if __name__ == "__main__":
 
     client = Client(
-        n_workers=5,
+        n_workers=10,
         threads_per_worker=1,
         dashboard_address=":3338",
         direct_to_workers=True,
     )
 
     ## Params
-    n_queues = 2
-    n_consumers = 2
+    n_queues = 10
+    n_consumers = 20
 
     queue_pool = QueuePool(client, n_queues)
 
@@ -28,7 +33,25 @@ if __name__ == "__main__":
 
     consumer_pool.start()
 
-    for i in range(5):
-        queue_pool.submit(process_item)
+    ## Perf count
+    s = time.perf_counter()
+
+    queue_pool.batch_submit([(inc, 1) for _ in range(10000)])
+
+    print(queue_pool)
+
+    e = time.perf_counter()
+
+    print("===============================================")
+    logger.info(f"Submitted all items in {e-s:.2f}s\n")
+    print("===============================================")
 
     consumer_pool.join(timestep=0.01, print_timestep=2, progress=True)
+
+    e = time.perf_counter()
+
+    print("===============================================")
+    logger.info(f"\n\nProcessed all items in {e-s:.2f}s")
+    print("===============================================")
+    print(queue_pool)
+    print(consumer_pool)

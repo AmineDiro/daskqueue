be3ca9e56efb9b1db63c5774890efcfa064102a8
amine dirhoussi <aminedirhoussi@amines-MacBook-Air.local>
2022-12-28 15:15:36 +0100

    color integration test

--- integration/test_integration_durable.py
+++ integration/test_integration_durable.py
@@ -1,9 +1,13 @@
 from time import perf_counter
 
+import click
+
 from daskqueue.Protocol import Message
 from daskqueue.queue.durable_queue import DurableQueue
 
 N = 1_000
+
+cprint = click.echo
 
 
 def rdx_msg():
@@ -24,10 +28,14 @@ def test_durable_queue(durable_queue: DurableQueue):
     e = perf_counter()
 
     w_ops = N / (e - s)  # op/s
-    print(f"Mean write ops [{N}tests] {w_ops:.2f} wop/s")
+    cprint("\n\t Mean write ops: " + click.style(f"{w_ops:.2f} wop/s", fg="green"))
 
+    s = perf_counter()
     for _ in range(N):
         msg = durable_queue.get_sync()
         get_msgs.append(msg.id)
+    e = perf_counter()
+    r_ops = N / (e - s)  # op/s
+    cprint("\n\t Mean read ops: " + click.style(f"{r_ops:.2f} rop/s", fg="green"))
 
     assert put_msgs == get_msgs
